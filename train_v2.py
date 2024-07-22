7fdf7eca121fffc79e871f877355e874cc0f8048
clamalo <89725263+clamalo@users.noreply.github.com>
2024-07-21 21:14:33 -0700

    various changes
    added custom date ranges for training to go along with custom train/test split. reorganized old test scripts into a folder (fix later). fixed plotting 10 random per test cycle, too.

--- train_v2.py
+++ train_v2.py
@@ -14,18 +14,20 @@ import matplotlib.patches as patches
 import warnings
 warnings.filterwarnings("ignore")
 
-BASE_DIR = '/Users/clamalo/documents/harpnet/load_data/'
-domain = 14
-ingest = True
 
-setup(domain)
+domain = 14
+ingest = False
+first_month = (2019, 1)
+last_month = (2020, 12)
+train_test_cutoff = '2020-10-01:00:00:00'
+
 
 if ingest:
+    setup(domain)
     create_grid_domains()
     xr_to_np(domain)
 
-train_test_cutoff = '2020-10-01:00:00:00'
-train_input_file_paths, train_target_file_paths, test_input_file_paths, test_target_file_paths = create_paths(train_test_cutoff, domain)
+train_input_file_paths, train_target_file_paths, test_input_file_paths, test_target_file_paths = create_paths(domain, first_month, last_month, train_test_cutoff)
 
 train_dataset, train_dataloader = create_dataloader(train_input_file_paths, train_target_file_paths)
 test_dataset, test_dataloader = create_dataloader(test_input_file_paths, test_target_file_paths, shuffle=False)
@@ -37,50 +39,10 @@ model = UNetWithAttention(1, 1, output_shape=(64,64)).to('mps')
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 criterion = nn.MSELoss()
 
-def train(model, dataloader, criterion, optimizer, device):
-    lats, lons, input_lats, input_lons = get_lats_lons(domain)
-    model.train()
-    running_loss = 0.0
-    for i, (inputs, targets) in tqdm(enumerate(dataloader), total=len(dataloader)):
-        inputs, targets = inputs.to(device), targets.to(device)
-        optimizer.zero_grad()
-        outputs = model(inputs).squeeze(1)
-        loss = criterion(outputs, targets)
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item()
-    return running_loss / len(dataloader)
-
-def test(model, dataloader, criterion, device):
-    lats, lons, input_lats, input_lons = get_lats_lons(domain)
-    model.eval()
-    running_loss = 0.0
-    for i, (inputs, targets) in tqdm(enumerate(dataloader), total=len(dataloader)):
-        inputs, targets = inputs.to(device), targets.to(device)
-        outputs = model(inputs).squeeze(1)
-        loss = criterion(outputs, targets)
-        running_loss += loss.item()
-
-        random_10 = np.random.randint(0, len(dataloader))
-        if i in [random_10]:
-            fig, axs = plt.subplots(1, 3, figsize=(12, 4), subplot_kw={'projection': cartopy.crs.PlateCarree()})
-            axs[0].pcolormesh(lons, lats, inputs[0].cpu().numpy(), transform=cartopy.crs.PlateCarree(), vmin=0, vmax=10)
-            axs[1].pcolormesh(lons, lats, outputs[0].cpu().detach().numpy(), transform=cartopy.crs.PlateCarree(), vmin=0, vmax=10)
-            axs[2].pcolormesh(lons, lats, targets[0].cpu().numpy(), transform=cartopy.crs.PlateCarree(), vmin=0, vmax=10)
-            for ax in axs:
-                ax.coastlines()
-                ax.add_feature(USCOUNTIES.with_scale('5m'), edgecolor='gray')
-            box = patches.Rectangle((lons[0], lats[0]), lons[-1] - lons[0], lats[-1] - lats[0],
-                                    linewidth=1, edgecolor='r', facecolor='none')
-            plt.savefig(f'figures/test/{i}.png')
-
-
-    return running_loss / len(dataloader)
-
 num_epochs = 5
 for epoch in range(num_epochs):
     train_loss = train(model, train_dataloader, criterion, optimizer, 'mps')
-    test_loss = test(model, test_dataloader, criterion, 'mps')
+    test_loss = test(domain, model, test_dataloader, criterion, 'mps')
     print(f'Epoch {epoch+1}/{num_epochs} - Train Loss: {train_loss:.4f} - Test Loss: {test_loss:.4f}')
     checkpoint = {
         'model_state_dict': model.state_dict(),
--- utils/utils2.py
+++ utils/utils2.py
@@ -1,18 +1,24 @@
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 import xarray as xr
 import pickle
 import pandas as pd
 from tqdm import tqdm
 import constants
+import matplotlib.pyplot as plt
+import cartopy
+from metpy.plots import USCOUNTIES
+import matplotlib.patches as patches
+
 
 def setup(domain):
     if not os.path.exists(f'{constants.domains_dir}'):
         os.makedirs(f'{constants.domains_dir}')
-    if not os.path.exists(f'{constants.domains_dir}{domain}/'):
-        os.makedirs(f'{constants.domains_dir}{domain}/')
+    if not os.path.exists(f'{constants.domains_dir}/{domain}/'):
+        os.makedirs(f'{constants.domains_dir}/{domain}/')
 
 
 def create_grid_domains():
@@ -88,10 +94,21 @@ def get_lats_lons(domain):
     return lats, lons, input_lats, input_lons
 
 
-def create_paths(train_test_cutoff,domain):
+def create_paths(domain, first_month, last_month, train_test_cutoff):
     train_test_cutoff = datetime.strptime(train_test_cutoff, '%Y-%m-%d:%H:%M:%S')
-    input_file_paths = sorted([os.path.join(f'{constants.domains_dir}{domain}/', fp) for fp in os.listdir(f'{constants.domains_dir}{domain}/') if fp.startswith('input') and fp.endswith('.npy')], key=lambda fp: os.path.basename(fp))
-    target_file_paths = sorted([os.path.join(f'{constants.domains_dir}{domain}/', fp) for fp in os.listdir(f'{constants.domains_dir}{domain}/') if fp.startswith('target') and fp.endswith('.npy')], key=lambda fp: os.path.basename(fp))
+    first_month = datetime(first_month[0], first_month[1], 1)
+    last_month = datetime(last_month[0], last_month[1], 1)
+
+    input_file_paths = []
+    target_file_paths = []
+
+    current_month = first_month
+    while current_month <= last_month:
+        input_fp = f'{constants.domains_dir}{domain}/input_{current_month.year}_{current_month.month:02d}.npy'
+        target_fp = f'{constants.domains_dir}{domain}/target_{current_month.year}_{current_month.month:02d}.npy'
+        input_file_paths.append(input_fp)
+        target_file_paths.append(target_fp)
+        current_month += relativedelta(months=1)
     train_input_file_paths = []
     train_target_file_paths = []
     test_input_file_paths = []
@@ -125,3 +142,44 @@ def create_dataloader(input_file_paths, target_file_paths, batch_size=64, shuffl
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
     return dataset, dataloader
 
+
+def train(model, dataloader, criterion, optimizer, device):
+    model.train()
+    running_loss = 0.0
+    for i, (inputs, targets) in tqdm(enumerate(dataloader), total=len(dataloader)):
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs).squeeze(1)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+    return running_loss / len(dataloader)
+
+
+def test(domain, model, dataloader, criterion, device):
+    lats, lons, input_lats, input_lons = get_lats_lons(domain)
+    model.eval()
+    running_loss = 0.0
+    for i, (inputs, targets) in tqdm(enumerate(dataloader), total=len(dataloader)):
+        inputs, targets = inputs.to(device), targets.to(device)
+        outputs = model(inputs).squeeze(1)
+        loss = criterion(outputs, targets)
+        running_loss += loss.item()
+
+        random_10 = np.random.randint(0, len(dataloader), 10)
+        plotted = 0
+        if i in random_10:
+            fig, axs = plt.subplots(1, 3, figsize=(12, 4), subplot_kw={'projection': cartopy.crs.PlateCarree()})
+            axs[0].pcolormesh(lons, lats, inputs[0].cpu().numpy(), transform=cartopy.crs.PlateCarree(), vmin=0, vmax=10)
+            axs[1].pcolormesh(lons, lats, outputs[0].cpu().detach().numpy(), transform=cartopy.crs.PlateCarree(), vmin=0, vmax=10)
+            axs[2].pcolormesh(lons, lats, targets[0].cpu().numpy(), transform=cartopy.crs.PlateCarree(), vmin=0, vmax=10)
+            for ax in axs:
+                ax.coastlines()
+                ax.add_feature(USCOUNTIES.with_scale('5m'), edgecolor='gray')
+            box = patches.Rectangle((lons[0], lats[0]), lons[-1] - lons[0], lats[-1] - lats[0],
+                                    linewidth=1, edgecolor='r', facecolor='none')
+            plt.savefig(f'figures/test/{plotted}.png')
+            plt.close()
+            plotted += 1
+    return running_loss / len(dataloader)
