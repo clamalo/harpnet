b13710ab4ce334b73beb13dec499f6ed58cb0030
clamalo <89725263+clamalo@users.noreply.github.com>
2024-12-23 23:14:09 -0800

    *

--- v3/fine_tuning.py
+++ v3/fine_tuning.py
@@ -86,6 +86,7 @@ if __name__ == "__main__":
     torch.manual_seed(RANDOM_SEED)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(RANDOM_SEED)
+        
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
     torch.use_deterministic_algorithms(True)
